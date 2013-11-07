2029ce3263d9fd972b67ddaa9cfc7d2a1ac82594
Skoviase <nobody@no.email>
2013-11-07 14:41:05 -0600

    added bootstrap, knockout, and jquery plus a basic container

--- views.py
+++ views.py
@@ -2,6 +2,11 @@ from annoying.decorators import render_to, ajax_request
 
 @render_to('templates/container.html')
 def draw_container(request):
+	ctx	=	{	'name': "Joe Blow",
+				'address': "3490 Jeffro Lane",
+				'city': "Austin",
+				'state': "Texas"
+			}
 
 	# uses render_to to draw the template
-	return dict()
+	return dict(customer=ctx)

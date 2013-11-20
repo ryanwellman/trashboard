c30e940deaf3a6f3314e8fd44cc5258f64b886f2
Skoviase <patrick@protectamerica.com>
2013-11-20 13:03:56 -0600

    added request timing middleware, greatly improved the form

--- agreement/models.py
+++ agreement/models.py
@@ -1,50 +1,75 @@
 from django.db import models
+from cPickle import loads, dumps
 
 class Agreement(models.Model):
 	"""
 	dummy model, contains a dict
 	"""
 
-	name = models.CharField(max_length=50)
+	fname = models.CharField(max_length=50)
+	lname = models.CharField(max_length=50)
+	initial = models.CharField(max_length=1)
 	address = models.CharField(max_length=80)
 	city = models.CharField(max_length=50)
 	state = models.CharField(max_length=25)
 	zip = models.CharField(max_length=10)
+	taxid = models.CharField(max_length=15)
+	email = models.CharField(max_length=75)
 	country = models.CharField(max_length=10)
 	approved = models.CharField(max_length=10)
-	one = models.CharField(max_length=10)
-	two = models.CharField(max_length=10)
-	three = models.CharField(max_length=10)
+	package = models.CharField(max_length=10)
+	shipping = models.CharField(max_length=10)
+	monitoring = models.CharField(max_length=10)
+	# this stores a serialized array
+	completed = models.TextField()
+
 
 	def __unicode__(self):
-		return "<" + self.__class__.__name__ + ":" + ','.join([self.name, self.address, self.city, self.state,
-							self.zip, self.country, self.approved, self.one, self.two, self.three]) + ">"
+		return ','.join(["{0} {1}. {2}".format(self.fname, self.initial, self.lname), self.approved, self.package, self.city, self.state])
 
 	def serialize(self):
+		# cpickle loads does not like blank textfields
+		# the obvious reason is that dumps([]) does not equal ''
+		try:
+			complete = loads(str(self.completed))
+		except EOFError as e:
+			complete = []
+
 		return 	{
-					"name": self.name,
+					"agreement_id": self.id, # this one is named differently in the javascript
+					"fname": self.fname,
+					"lname": self.lname,
+					"initial": self.initial,
 					"address": self.address,
 					"city": self.city,
 					"state": self.state,
 					"zip": self.zip,
+					"taxid": self.taxid,
+					"email": self.email,
 					"country": self.country,
 					"approved": self.approved,
-					"one": self.one,
-					"two": self.two,
-					"three": self.three,
+					"package": self.package,
+					"shipping": self.shipping,
+					"monitoring": self.monitoring,
+					"completed": complete,
 				}
 
 	def update_from_dict(self, incoming):
-		self.name = incoming.get('name')
+		self.fname = incoming.get('fname')
+		self.lname = incoming.get('lname')
+		self.initial = incoming.get('initial')
 		self.address = incoming.get('address')
 		self.city = incoming.get('city')
 		self.state = incoming.get('state')
 		self.zip = incoming.get('zip')
+		self.taxid = incoming.get('taxid')
+		self.email = incoming.get('email')
 		self.country = incoming.get('country')
 		self.approved = incoming.get('approved')
-		self.one = incoming.get('one')
-		self.two = incoming.get('two')
-		self.three = incoming.get('three')
+		self.package = incoming.get('package')
+		self.shipping = incoming.get('shipping')
+		self.monitoring = incoming.get('monitoring')
+		self.completed = dumps(incoming.get('completed'))
 		self.save()
 
 	class Meta:

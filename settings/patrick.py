6c2715c170e7f5d37d5bb544a024ddd6d4c4adae
Skoviase <patrick@protectamerica.com>
2013-12-11 16:31:17 -0600

    my settings file

---
+++ settings/patrick.py
@@ -0,0 +1,35 @@
+import os, sys, socket
+from devel import *
+
+SITE_URL = 'http://greatjustice:5001'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'trashboard',
+        'USER': 'trashboard',
+        'PASSWORD': 'trashboard',
+        'HOST': 'IT14-NEW\BYAKKO',
+        'PORT': 1433,
+        'OPTIONS': {
+            'driver': 'FreeTDS',
+            'host_is_server': True,
+            'extra_params': 'TDS_Version=7.2;APP=Digitals',
+            'unicode_results': True,
+            'driver_needs_utf8': False,
+        },
+    }
+}
+
+
+INTERNAL_IPS += (('209.86.113.21'),)
+
+INSTALLED_APPS += ('django_extensions',)
+
+
+# EMAIL_HOST = '358607-exch1.protectamerica.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#COMPRESS_ENABLED = True

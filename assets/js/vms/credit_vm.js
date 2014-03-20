77a36c573a202da0147f67af8fd61b81fb49fbdf
Colin Fletcher <cfletcher1856@gmail.com>
2014-03-20 13:38:16 -0500

    Happy with the way it looks now, sans navbar

--- assets/js/vms/credit_vm.js
+++ assets/js/vms/credit_vm.js
@@ -20,8 +20,28 @@ CreditVM = function(master) {
             }
         });
         cf.which = which.substring(0,1).toUpperCase() + which.substring(1);
+        cf.label_display_text = ko.computed(function(){
+            var status = cf.credit_status();
+
+            if(!status){
+                status = 'NOT RUN';
+            }
+
+            // jQuery object to string
+            var icon = $("<i/>", {
+                'class': window.CREDIT_LABEL_ICON_OPTIONS[status]
+            }).wrap('<div/>').parent().html();
+
+            return icon + " " + status;
+        });
         cf.label_display = ko.computed(function(){
-            return window.CREDIT_LABEL_OPTIONS[cf.credit_status()];
+            var status = cf.credit_status();
+
+            if(!status){
+                status = 'NOT RUN';
+            }
+
+            return window.CREDIT_LABEL_OPTIONS[status];
         });
         return cf;
     }

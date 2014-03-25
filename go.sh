dd7378de9628277c6b6d976414597517be837526
Brian Arsuaga <brian.arsuaga@gmail.com>
2014-03-25 00:46:48 -0500

    fixed go.sh order

--- go.sh
+++ go.sh
@@ -1,10 +1,10 @@
 echo "no" | ./manage.py syncdb
 ./manage.py migrate
 ./manage.py fixtures
+./import_live_campaigns.py
 echo Loading zipcodes, takes awhile.
 ./manage.py loaddata regional/fixtures/us_zipcode_entries.json
 echo Done loading zipcodes
 ./manage.py runserver 0:7001
-./import_live_campaigns.py
 
 

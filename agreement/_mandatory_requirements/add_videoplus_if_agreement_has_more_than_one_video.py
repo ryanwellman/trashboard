15c4da0e4a88409160d1ca3483e205bf7e9476a6
Skoviase <patrick@protectamerica.com>
2014-03-25 14:25:18 -0500

    added the videoplus item to every price table in fixtures.py and made the promo look for CAMERA instead

--- agreement/_mandatory_requirements/add_videoplus_if_agreement_has_more_than_one_video.py
+++ agreement/_mandatory_requirements/add_videoplus_if_agreement_has_more_than_one_video.py
@@ -2,5 +2,5 @@ from _mandatory_requirement import MandatoryRequirement
 
 class VideoPlusIfMoreThanOneCamera(MandatoryRequirement):
     def check(self):
-        if self.total_quantities['VIDEO'] > 1 and not self.total_quantities['VIDEOPLUS']:
+        if self.total_quantities['CAMERA'] > 1 and not self.total_quantities['VIDEOPLUS']:
             self.add_mandatory_product('VIDEOPLUS', 1)
